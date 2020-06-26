8901da8a4a3854264391e5ef5b46043fb85a3d72
daniboomerang <estevez.dani@gmail.com>
2020-06-26 11:41:22 +0700

    Install and setup Jest/Enzyme.
    Add test for App component.

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it('should render App component', () => {
+  const wrapper = mount(<App />);
 
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  expect(wrapper.text()).toBe('Address book app');
 });
